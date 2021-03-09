f85e271c6e4c2a7d08b80c74fdd6f813a11d2a37
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-03-09 17:02:10 +0100

    Removes cryptoswift from first target

--- Package.swift
+++ Package.swift
@@ -10,9 +10,10 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Data4LifeSDKUtils",
-            targets: ["Data4LifeSDKUtils", "CryptoSwift"]),
-        .library(name: "CryptoSwift",
-                 targets: ["CryptoSwift"]),
+            targets: ["Data4LifeSDKUtils"]),
+        .library(
+            name: "CryptoSwift",
+            targets: ["CryptoSwift"]),
     ],
     targets: [
         .binaryTarget(
