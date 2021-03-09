0de4323433cedeaeeb6a221b69310c504047be1a
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-03-09 16:13:47 +0100

    Removes fastlane, carthage support and pushes first version of package.swift using the xcframework (ikely to fail)

--- Package.swift
+++ Package.swift
@@ -10,20 +10,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Data4LifeSDKUtils",
-            type: .dynamic,
             targets: ["Data4LifeSDKUtils"]),
     ],
-    dependencies: [
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.7"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
+            name: "CryptoSwift",
+            path: "xcframeworks/CryptoSwift.xcframework"
+        ),
+        .binaryTarget(
             name: "Data4LifeSDKUtils",
-            dependencies: ["CryptoSwift"],
-            path: "Sources",
-            exclude: ["SDKUtils/Info.plist"]),
+            path: "xcframeworks/Data4LifeSDKUtils.xcframework"
+        ),
         .testTarget(
             name: "Data4LifeSDKUtilsTests",
             dependencies: ["Data4LifeSDKUtils"],
