75415f9a87d432de76cfeca7d9477bd434cab989
Alessio Borraccino <alessio.borraccino@data4life.care>
2021-11-10 14:15:52 +0100

    removes warning on exclude

--- Package.swift
+++ Package.swift
@@ -16,9 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "Data4LifeSDKUtils",
             url: "https://d4l-ios-artifact-repository.s3.eu-central-1.amazonaws.com/d4l-data4life/d4l-utils-ios/Data4LifeSDKUtils-xcframework-v0.7.0.zip",
-            checksum: "2021-11-10 14:11:31.755 xcodebuild[96911:4065556] [MT] DVTPlugInManager: Required plug-in compatibility UUID F56A1938-53DE-493D-9D64-87EE6C415E4D for Kotlin.ideplugin (org.kotlinlang.xcode.kotlin) not present
-2021-11-10 14:11:32.336 xcodebuild[96912:4065692] [MT] DVTPlugInManager: Required plug-in compatibility UUID F56A1938-53DE-493D-9D64-87EE6C415E4D for Kotlin.ideplugin (org.kotlinlang.xcode.kotlin) not present
-91a2d43b886a827b6f1df6775196bef1a394a0a3f2ed1de412338b0604633495"
+            checksum: "91a2d43b886a827b6f1df6775196bef1a394a0a3f2ed1de412338b0604633495"
         ),
         .testTarget(
             name: "Data4LifeSDKUtilsTests",
@@ -26,11 +24,6 @@ let package = Package(
             path: "Tests",
             exclude: [
                 "SDKUtilsTests/Info.plist",
-                "build",
-                "fastlane",
-                "vendor",
-                "Gemfile",
-                "Gemfile.lock"
             ],
             resources: [
                 .copy("SDKUtilsTests/Resources/Data Samples/sample-jfif.jpg"),
